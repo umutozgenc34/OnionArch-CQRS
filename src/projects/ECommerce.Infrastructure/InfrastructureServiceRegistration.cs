60b5d5f41caaf0c65ec40c5331d373e818a00416
umutozgenc34 <umutozgenc34@hotmail.com>
2024-12-24 22:06:19 +0300

    try

---
+++ src/projects/ECommerce.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Application.Services.Infrastructure;
+using ECommerce.Infrastructure.CloudinaryServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Infrastructure;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureDependencies(this IServiceCollection service, IConfiguration configuration)
+    {
+        service.AddScoped<ICloudinaryService, CloudinaryService>();
+        service.Configure<CloudinarySettings>(configuration.GetSection("CloudinarySettings"));
+        return service;
+    }
+}
